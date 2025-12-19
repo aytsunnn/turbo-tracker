d339c3d54897df7f3f31073e1a3a333c82176660
aytsunnn <lohkinaas@gmail.com>
2025-12-19 18:06:14 +0400

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '../App.vue'
-import Dashboard from '../Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +11,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: App,
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    meta: { requiresAuth: true }, // можно добавить защиту
   },
 ]
 
